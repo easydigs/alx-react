32c742eb43d3598f7d8f395eb09c3432b1e18217
easydigs <easydigitalkoncept@gmail.com>
2024-08-21 08:16:24 +0100

    Task 3

---
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/dashboard_main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i, // Process CSS files
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i, // Process image files
+        type: 'asset/resource',
+        use: [
+          {
+            loader: 'image-webpack-loader', // Optimize images
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              optipng: {
+                enabled: true,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4,
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              webp: {
+                quality: 75,
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  mode: 'development', // Set mode to development
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'), // Serve static files from 'public'
+    },
+    port: 8564, // Set the port to 8564
+    open: true, // Automatically open the browser
+  },
+};
