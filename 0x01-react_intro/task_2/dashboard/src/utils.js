c28ab457a0f5599177a26a9b0740935cada3592f
easydigs <easydigitalkoncept@gmail.com>
2024-08-22 21:05:20 +0100

    Task 4. Modify the Notifications

--- 0x01-react_intro/task_2/dashboard/src/utils.js
+++ 0x01-react_intro/task_2/dashboard/src/utils.js
@@ -11,7 +11,12 @@ const getFooterCopy = (isIndex) => {
 	}
 }
 
+const getLatestNotification = () => {
+	return "<strong>Urgent requirement</strong> - complete by EOD";
+};
+
 module.exports = {
+	getLatestNotification,
 	getFooterCopy,
 	getFullYear,
 };
