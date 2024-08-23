58213049e3afff0187130b8c191ec358d27dd206
easydigs <easydigitalkoncept@gmail.com>
2024-08-23 10:42:44 +0100

    Initial gh-pages commit

--- 0x00-Webpack/task_3/js/footer.js
+++
@@ -1,6 +0,0 @@
-import $ from 'jquery';
-import '../css/footer.css';
-
-$(document).ready(function() {
-  $('body').append('<p>Copyright - Holberton School</p>');
-});
--- 0x00-Webpack/task_3/js/header.js
+++
@@ -1,9 +0,0 @@
-import $ from 'jquery';
-import '../css/header.css';
-
-console.log('Init header');
-
-$(document).ready(function() {
-  $('body').prepend('<div id="logo"></div>');
-  $('body').prepend('<h1>Holberton Dashboard</h1>');
-});
