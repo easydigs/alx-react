0f1143518df10bb8eb6c0e0a153a4ea26daabce1
easydigs <easydigitalkoncept@gmail.com>
2024-08-23 09:29:17 +0100

    Task 7 7. Create React tests

--- 0x01-react_intro/task_3/dashboard/src/App.test.js
+++ 0x01-react_intro/task_3/dashboard/src/App.test.js
@@ -1,11 +1,26 @@
-// src/App.test.js
 import React from 'react';
-import { shallow } from 'enzyme';
 import App from './App';
+import { shallow } from 'enzyme';
 
-describe('<App />', () => {
-  it('renders without crashing', () => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.exists()).toBe(true);
-  });
+describe('App tests', () => {
+	it('renders without crashing', () => {
+		const component = shallow(<App />);
+
+		expect(component).toBeDefined();
+	});
+	it('should render a div with the class App-header', () => {
+		const component = shallow(<App />);
+
+		expect(component.find('.App-header')).toBeDefined();
+	});
+	it('should render a div with the class App-body', () => {
+		const component = shallow(<App />);
+
+		expect(component.find('.App-body')).toBeDefined();
+	});
+	it('should render a div with the class App-footer', () => {
+		const component = shallow(<App />);
+
+		expect(component.find('.App-footer')).toBeDefined();
+	});
 });
