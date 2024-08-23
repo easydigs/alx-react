d3ec1deace07bdb4203b79f1e0b6082f4086eb34
easydigs <easydigitalkoncept@gmail.com>
2024-08-23 09:09:37 +0100

    Task 6. Install Enzyme

--- 0x01-react_intro/task_3/dashboard/src/App.test.js
+++ 0x01-react_intro/task_3/dashboard/src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
+// src/App.test.js
+import React from 'react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.exists()).toBe(true);
+  });
 });
