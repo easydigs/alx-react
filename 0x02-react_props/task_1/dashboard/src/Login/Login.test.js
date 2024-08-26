a96dbb2cb4126f7f9d4a84b0a169c1198f1a245d
easydigs <easydigitalkoncept@gmail.com>
2024-08-26 07:48:13 +0100

    Task 1. update the login test file

--- 0x02-react_props/task_1/dashboard/src/Login/Login.test.js
+++ 0x02-react_props/task_1/dashboard/src/Login/Login.test.js
@@ -7,6 +7,15 @@ const wrapper = shallow(<Login />);
 it('renders without crashing', () => {
   shallow(<Login />);
 });
+
 it('renders login', () => {
   expect(wrapper.find('main.login').exists()).toEqual(true);
+});
+
+it('renders two input tags', () => {
+  expect(wrapper.find('input').length).toEqual(2);
+});
+
+it('renders two label tags', () => {
+  expect(wrapper.find('label').length).toEqual(2);
 });
