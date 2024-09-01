1cef5fdcde22383f593c46d94c5862f031f0fa4b
easydigs <easydigitalkoncept@gmail.com>
2024-09-01 13:21:48 +0100

    updated

--- 0x02-react_props/task_4/dashboard/src/App/App.js
+++ 0x02-react_props/task_4/dashboard/src/App/App.js
@@ -1,21 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types'; // Import PropTypes
 import './App.css';
 import Header from '../Header/Header';
 import Login from '../Login/Login';
 import Footer from '../Footer/Footer';
 import Notifications from '../Notifications/Notifications';
-import NotificationItem from '../Notifications/NotificationItem';
+import CourseList from '../CourseList/CourseList';
 
-
-function App() {
+function App({ isLoggedIn }) {
   return (
     <>
       <Notifications />
       <Header />
-      <Login />
+      {isLoggedIn ? <CourseList /> : <Login />}
       <Footer />
     </>
   );
 }
+
+// Set default props
+App.defaultProps = {
+  isLoggedIn: false,
+};
+
+// Define prop types
+App.propTypes = {
+  isLoggedIn: PropTypes.bool,
+};
 
 export default App;
