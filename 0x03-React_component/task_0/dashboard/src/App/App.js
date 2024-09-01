fac733275e116ce7c88b151ad21e77731c78db36
easydigs <easydigitalkoncept@gmail.com>
2024-09-01 14:04:33 +0100

    task 0

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
-import '../App.css';
+import React from 'react';
+import './App.css';
 import Header from '../Header/Header';
 import Login from '../Login/Login';
 import Footer from '../Footer/Footer';
 import Notifications from '../Notifications/Notifications';
+import CourseList from '../CourseList/CourseList';
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <Notifications />
-        <Header />
-        <Login />
-        <Footer />
-      </>
-    );
-  }
+const listCourses = [
+  { id: 1, name: 'ES6', credit: 60 },
+  { id: 2, name: 'Webpack', credit: 20 },
+  { id: 3, name: 'React', credit: 40 },
+];
+
+function App() {
+  return (
+    <>
+      <Notifications />
+      <Header />
+      <Login />
+      <CourseList listCourses={listCourses} />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
