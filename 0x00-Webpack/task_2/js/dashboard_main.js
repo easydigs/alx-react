a613198b9efdd90bc911d044c3518833a1a5e2b1
easydigs <easydigitalkoncept@gmail.com>
2024-10-28 09:20:54 +0100

    Task 2 updated

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -2,20 +2,17 @@ import '../css/main.css';
 import $ from 'jquery';
 import _ from 'lodash';
 
-let count = 0;  // Initialize count outside of the function
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+let count = 0;
 
 function updateCounter() {
   count++;
-  $('#count').text(`${count} clicks on the button`);
-}
+  $("#count").html(`${count} clicks on the button`);
+};
 
-$(document).ready(function() {
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<button id="click-btn">Click here to get started</button>');
-  $('body').append('<p id="count"></p>');
-  $('body').append('<p>Copyright - Holberton School</p>');
-
-  const debouncedUpdate = _.debounce(updateCounter, 300);
-  $('#click-btn').on('click', debouncedUpdate);
-});
+$('button').on('click', _.debounce(updateCounter, 500));
